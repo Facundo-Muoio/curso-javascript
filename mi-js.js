0a180cec08bbb4626278f1c44889c5c9fdd84713
Facundo Muoio <facundo.muoio@gmail.com>
2021-09-01 16:44:59 -0300

    repositorio para entrega de proyectos

---
+++ mi-js.js
@@ -0,0 +1,11 @@
+let apellido = prompt("Ingresa apellido");
+alert(apellido);
+
+let edad = prompt("Ingresa edad");
+let años = parseInt(edad);
+
+let numero = 20;
+
+let resultado = años + numero;
+ 
+alert(resultado);

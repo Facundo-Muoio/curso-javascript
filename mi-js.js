28e31e1e2fefa3d8f4016f269b81bec22f06c9e2
Facundo Muoio <facundo.muoio@gmail.com>
2021-09-04 00:44:51 -0300

    segundo crear un algoritmo con un condicional

--- mi-js.js
+++ mi-js.js
@@ -1,11 +1,38 @@
-let apellido = prompt("Ingresa apellido");
-alert(apellido);
+// let apellido = prompt("Ingresa apellido");
+// alert(apellido);
 
-let edad = prompt("Ingresa edad");
-let años = parseInt(edad);
+// let edad = prompt("Ingresa edad");
+// let años = parseInt(edad);
 
-let numero = 20;
+// let numero = 20;
 
-let resultado = años + numero;
+// let resultado = años + numero;
  
-alert(resultado);
+// alert(resultado);
+
+
+let numero = prompt ("Ingrese un número");
+numeroInt = parseInt (numero);
+
+
+if (numeroInt < 10){
+    alert("El número es menor a 10");
+}
+
+else if (numeroInt == 10) {
+    alert("El número es 10");
+} 
+
+else if((numeroInt > 10) && (numeroInt < 50)) {
+    alert("El número esta entre 10 y 50");
+}
+
+else if (numeroInt == 50) {
+    alert("El número es igual a 50");
+}
+
+else {
+    alert("El número es mayor a 50");
+}
+
+

b5c1abb40f5ca59a0f8508ff661270cbab84e921
Facundo Muoio <facundo.muoio@gmail.com>
2021-09-20 21:10:35 -0300

    Desafio: incorporar arrays

--- mi-js.js
+++ mi-js.js
@@ -614,46 +614,180 @@
 
 // console.log(torta)
 
-let selectPlan;
-let cantidadDeMeses;
-let costoFinal;
+// let selectPlan;
+// let cantidadDeMeses;
+// let costoFinal;
 
-alert("Esta es una lista con nuestros planes. \nDiario Digital\nPrecio mensual: $100\nBeneficios:\n1- Acceso a todo el contenido digital del diario  \nClub Gol\nPrecio mensual: $150\nBeneficios:\n1- Acceso a todo el contenido digital del dirario \n2- Tarjeta del Club Gol con descuentos en productos y servicios \nDiario del Domingo\nPrecio mensual: $200\nBeneficios:\n1- Acceso a todo el contenido digital del dirario\n2- Tarjeta del Club Gol con descuentos en productos y servicios \n3- Entrega del Diario del Domingo en tu casa");
-selectPlan = prompt("Ingrese el plan al que desee suscribirse. Planes: \ndiario digital \nclub gol \ndiario del domingo");
-selectPlan = selectPlan.toLowerCase();
-cantidadDeMeses = prompt("Ingrese el número de meses a las cuales desea suscribirse");
+// alert("Esta es una lista con nuestros planes. \nDiario Digital\nPrecio mensual: $100\nBeneficios:\n1- Acceso a todo el contenido digital del diario  \nClub Gol\nPrecio mensual: $150\nBeneficios:\n1- Acceso a todo el contenido digital del dirario \n2- Tarjeta del Club Gol con descuentos en productos y servicios \nDiario del Domingo\nPrecio mensual: $200\nBeneficios:\n1- Acceso a todo el contenido digital del dirario\n2- Tarjeta del Club Gol con descuentos en productos y servicios \n3- Entrega del Diario del Domingo en tu casa");
+// selectPlan = prompt("Ingrese el plan al que desee suscribirse. Planes: \ndiario digital \nclub gol \ndiario del domingo");
+// selectPlan = selectPlan.toLowerCase();
+// cantidadDeMeses = prompt("Ingrese el número de meses a las cuales desea suscribirse");
 
-class DiarioSuscripciones{
-    constructor(Nombre, Precio, Beneficios){
-        this.Nombre = Nombre;   
-        this.Precio = Precio;
-        this.Beneficios = Beneficios;
-    }
+// class DiarioSuscripciones{
+//     constructor(Nombre, Precio, Beneficios){
+//         this.Nombre = Nombre;   
+//         this.Precio = Precio;
+//         this.Beneficios = Beneficios;
+//     }
 
-    suscribirse(selectPlan) {
-        switch (selectPlan) {
-            case ("diario digital"):
-                costoFinal = 100 * cantidadDeMeses;
-                return "Usted se suscribio al plan Diario Digital por " + cantidadDeMeses + " meses." + " El costo final de su suscripcion es de: $" + costoFinal;
-            case ("club gol"):
-                costoFinal = 150 * cantidadDeMeses;
-                return "Usted se suscribio al plan Club Gol por " + cantidadDeMeses + " meses." + " El costo final de su suscripcion es de: $" + costoFinal;
-            case ("diario del domingo"):
-                costoFinal = 200 * cantidadDeMeses;
-                return "Usted se suscribio al plan Diario del Domingo por " + cantidadDeMeses + " meses." + " El costo final de su suscripcion es de: $" + costoFinal;
-        }
+//     suscribirse(selectPlan) {
+//         switch (selectPlan) {
+//             case ("diario digital"):
+//                 costoFinal = 100 * cantidadDeMeses;
+//                 return "Usted se suscribio al plan Diario Digital por " + cantidadDeMeses + " meses." + " El costo final de su suscripcion es de: $" + costoFinal;
+//             case ("club gol"):
+//                 costoFinal = 150 * cantidadDeMeses;
+//                 return "Usted se suscribio al plan Club Gol por " + cantidadDeMeses + " meses." + " El costo final de su suscripcion es de: $" + costoFinal;
+//             case ("diario del domingo"):
+//                 costoFinal = 200 * cantidadDeMeses;
+//                 return "Usted se suscribio al plan Diario del Domingo por " + cantidadDeMeses + " meses." + " El costo final de su suscripcion es de: $" + costoFinal;
+//         }
+//     }
+// }
+
+
+// const diarioDigital = new DiarioSuscripciones("Diario Digital","$100 Mensuales","Acceso a todo el contenido digital del diario");
+// const clubGol = new DiarioSuscripciones("Club Gol", "$150 Mensuales","1-Acceso a todo el contenido digital del dirario 2-Tarjeta del Club Gol con descuentos en productos y servicios");
+// const diarioDomingo = new DiarioSuscripciones("Diario de Domingo","$200 Mensuales","1-Acceso a todo el contenido digital del dirario 2-Tarjeta del Club Gol con descuentos en productos y servicios -3 Entrega del Diario del Domingo en tu casa");
+
+// console.log("A continuacion le dejamos nuestro planes detallados por si quiere verlos nuevamente")
+// console.log(diarioDigital);
+// console.log(clubGol);
+// console.log(diarioDomingo); 
+// console.log(diarioDigital.suscribirse(selectPlan));
+// alert("En la consola podra ver su suscripción");
+
+// const numeros = [1,2,3,4,5];
+// const porDos = numeros.map(numero => numero * 2);
+// console.log(numeros);
+// console.log(porDos);
+// console.log(numeros.indexOf(3));
+
+
+// const productos = [
+//     {id:1, producto:"arroz", precio:125},
+//     {id:2, producto:"fideo", precio:70},
+//     {id:3, producto:"pan", precio:50},
+//     {id:4, producto:"flan", precio: 100}
+// ];
+
+
+// const buscarPan = productos.find(producto => producto.id === 3);
+// console.log(buscarPan);
+
+// const baratos = productos.filter(producto => producto.precio < 70);
+// console.log(baratos);
+
+// const aumentos = productos.map(producto => producto.precio += 20 * 2);
+// console.log(aumentos);
+
+let opcionSecundaria;
+
+class Productos{
+    constructor(id,marca,nombre,precio){
+        this.id = id;
+        this.marca = marca;
+        this.nombre = nombre;
+        this.precio = precio; 
     }
 }
 
+const teclado1 = new Productos(01254,"HyperX","Alloy Core RGB",4495);
+const teclado2 = new Productos(45641,"HyperX","Alloy FPS Pro Mecanico Eng Switch Red",8490);
+const teclado3 = new Productos(56441,"HyperX","Alloy Elite 2 RBG Red",13799);
 
-const diarioDigital = new DiarioSuscripciones("Diario Digital","$100 Mensuales","Acceso a todo el contenido digital del diario");
-const clubGol = new DiarioSuscripciones("Club Gol", "$150 Mensuales","1-Acceso a todo el contenido digital del dirario 2-Tarjeta del Club Gol con descuentos en productos y servicios");
-const diarioDomingo = new DiarioSuscripciones("Diario de Domingo","$200 Mensuales","1-Acceso a todo el contenido digital del dirario 2-Tarjeta del Club Gol con descuentos en productos y servicios -3 Entrega del Diario del Domingo en tu casa");
+const teclado4 = new Productos(46529,"Logitech","G G213 Retroiluminado Multimedia",6790);
+const teclado5 = new Productos(32529,"Logitech","G PRO KDA RGB",11990);
+const teclado6 = new Productos(82129,"Logitech","G 915 RGB lightspeed",23490);
 
-console.log("A continuacion le dejamos nuestro planes detallados por si quiere verlos nuevamente")
-console.log(diarioDigital);
-console.log(clubGol);
-console.log(diarioDomingo); 
-console.log(diarioDigital.suscribirse(selectPlan));
-alert("En la consola podra ver su suscripción");
+const teclado7 = new Productos(26529,"Corsair","K55 RGB",7990);
+const teclado8 = new Productos(32529,"Corsair","K57 RGB WIRELESS",12990);
+const teclado9 = new Productos(82129,"Corsair","K70 RGB MK.2 Cherry MX-red",19890);
 
+const teclados = [teclado1,teclado2,teclado3,teclado4,teclado5,teclado6,teclado7,teclado8,teclado9];
+console.log("Productos filtrados que conservan su precio sin descuentos:");
+
+let opcion = parseInt(prompt("Puedo filtrar nuestro productos según su precio o marca, eliga la opcion que desee escribiendo el número de la misma.\n1-Precio\n2-marca"));
+while (opcion < 1 || opcion > 2){
+    alert("la opcion que usted esta ingresando no existe.")
+    opcion = parseInt(prompt("eliga la opcion que desee escribiendo el número de la misma.\n1-Precio\n2-marca"))
+} if (opcion === 1){
+    opcionSecundaria = parseInt(prompt("usted puede filtrar los productos por los siguientes rangos de precios\n1-Menores a $8000\n2-Mayores a $8000\n3-Mayores $13000"));
+    while (opcionSecundaria < 1 || opcionSecundaria > 3){
+        alert("la opcion que usted esta ingresando no existe.")
+        opcionSecundaria = parseInt(prompt("eliga la opcion que desee escribiendo el número de la misma\n1-Menores a $8000\n2-Mayores a $8000\n3-Mayores $13000"))
+    }
+    switch(opcionSecundaria){
+        case 1:
+            const tecladosBaratos = teclados.filter(teclado => teclado.precio < 8000);
+            console.log(tecladosBaratos);
+            break;
+        
+        case 2:
+            const tecladosGamaMedia = teclados.filter(teclado => teclado.precio > 8000);
+            console.log(tecladosGamaMedia);
+            break;
+
+        case 3:
+            const tecladosCaros = teclados.filter(teclado => teclado.precio > 13000);
+            console.log(tecladosCaros);
+            break;
+    }
+} else if(opcion === 2){
+    let opcionSecundaria = parseInt(prompt("Ingrese el número de opción para seleccionar productos según su marca\n1-HyperX\n2-Logitech\n3-Corsair"));
+    while (opcionSecundaria < 1 || opcionSecundaria > 3){
+        alert("la opcion que usted esta ingresando no existe.")
+        opcionSecundaria = parseInt(prompt("eliga la opcion que desee escribiendo el número de la misma\n1-HyperX\n2-Logitech\n3-Corsair"))
+    }
+    switch(opcionSecundaria){
+        case 1:
+            const filter1 = teclados.filter(marca => marca.marca === "HyperX");
+            console.log(filter1);
+            break;
+        
+        case 2:
+            const filter2 = teclados.filter(marca => marca.marca === "Logitech");
+            console.log(filter2);
+            break;
+
+        case 3:
+            const filter3 = teclados.filter(marca => marca.marca === "Corsair");
+            console.log(filter3);
+            break;
+    }
+}
+
+let comprar = parseInt(prompt("Ahora que estuvo viendo nuestros productos puede acceder a los mismos con el descuentro de nuestro hotsale.\n1-Si quiere acceder al catalogo con los descuestos del hotsale\n2-Sino quiere comprar nada"));
+while (comprar < 1 || comprar > 2){
+    alert("la opcion que usted esta ingresando no existe.")
+    opcionSecundaria = parseInt(prompt("eliga la opcion que desee escribiendo el número de la misma\n1-Si quiere acceder al catalogo con los descuestos del hotsale\n2-Sino quiere comprar nada"))
+}
+if (comprar === 1){
+    const tecladoD1 = new Productos(01254,"HyperX","Alloy Core RGB",4495);
+    const tecladoD2 = new Productos(45641,"HyperX","Alloy FPS Pro Mecanico Eng Switch Red",8490);
+    const tecladoD3 = new Productos(56441,"HyperX","Alloy Elite 2 RBG Red",13799);
+
+    const tecladoD4 = new Productos(46529,"Logitech","G G213 Retroiluminado Multimedia",6790);
+    const tecladoD5 = new Productos(32529,"Logitech","G PRO KDA RGB",11990);
+    const tecladoD6 = new Productos(82129,"Logitech","G 915 RGB lightspeed",23490);
+
+    const tecladoD7 = new Productos(26529,"Corsair","K55 RGB",7990);
+    const tecladoD8 = new Productos(32529,"Corsair","K57 RGB WIRELESS",12990);
+    const tecladoD9 = new Productos(82129,"Corsair","K70 RGB MK.2 Cherry MX-red",19890);
+
+    let tecladoDescuento = [tecladoD1,tecladoD2,tecladoD3,tecladoD4,tecladoD5,tecladoD6,tecladoD7,tecladoD8,tecladoD9]
+    let descuento = tecladoDescuento.map(producto => producto.precio - (producto.precio * 0.25));
+    tecladoDescuento[0]["precio"] = descuento[0];
+    tecladoDescuento[1]["precio"] = descuento[1];
+    tecladoDescuento[2]["precio"] = descuento[2];
+    tecladoDescuento[3]["precio"] = descuento[3];
+    tecladoDescuento[4]["precio"] = descuento[4];
+    tecladoDescuento[5]["precio"] = descuento[5];
+    tecladoDescuento[6]["precio"] = descuento[6];
+    tecladoDescuento[7]["precio"] = descuento[7];
+    tecladoDescuento[8]["precio"] = descuento[8];
+    console.log("Productos con descuento del % 25 del hotsale:")
+    console.log(tecladoDescuento);
+}else{
+    alert("Gracias por visitar nuestra tienda, Vuelva pronto");
+}
